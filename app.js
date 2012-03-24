12d16da3dcc0ae634b9065828860cd4d70550771
Aaron Tidwell <atidwell@businessinsider.com>
2012-03-23 23:30:01 -0400

    removed &

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ var restartServer = function(res) {
 			console.log('killed '+pidCount+' out of '+pids.length);
 			if (pidCount === pids.length) {
 				res.write('Server Processes Killed \n');
-				var cmd = 'cd '+serverPath+'gameserver/orangebox/;./srcds_run -game tf -autoupdate -maxplayers 24 +map cp_badlands &;'
+				var cmd = 'cd '+serverPath+'gameserver/orangebox/;./srcds_run -game tf -autoupdate -maxplayers 24 +map cp_badlands &'
 				console.log('cmd',cmd)
 				exec(cmd,function (error, stdout, stderr) {	  
 					if (error) {
