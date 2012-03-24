38ada4381defb2c64d5039371a9f9d9aa389796a
Aaron Tidwell <atidwell@businessinsider.com>
2012-03-23 23:24:54 -0400

    more

--- app.js
+++ app.js
@@ -11,16 +11,7 @@ var exec = require('child_process').exec;
 /* A simple secured web server, unauthenticated requests are not allowed */
 httpdigest.createServer(username, password, function(req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});		
-  res.write('Updating Server... \n')
-  runUpdate(function(error,stdout) {
-  	if (error) {
-  		handleError(res, error);
-  	}
-  	else {
-  		res.write(stdout);
-  		restartServer(res);
-  	}
-  })
+  runUpdate(res)
 }).listen(8000);
 
 
@@ -30,18 +21,26 @@ var handleError = function(res, error) {
 }
 
 //run exec to update server, piping stdout to the http response
-var runUpdate = function(cb) {
+var runUpdate = function(res) {
 	var child;
-
+	res.write('Updating Server... \n')
 	// executes `pwd`
 	child = exec("cd "+serverPath+"; ./steam -command update -game tf -dir gameserver;", function (error, stdout, stderr) {	  
-	  cb(error,stdout)
+	  	if (error) {
+  			handleError(res, error);
+  		}
+  		else {
+  			res.write(stdout);
+  			res.write('\nServer Update Complete')
+  			restartServer(res);
+  		}
 	});
 }
 
 var restartServer = function(res) {
-    console.log('restarting server...')
+    res.write('\nKilling Current Server Processes');
     psGrep.findPid('srcds',function(error,pids){
+    	if (!pids.length) {return;}
     	console.log('found',pids);
     	if (error) {
     		handleError(res, error);
@@ -61,13 +60,17 @@ var restartServer = function(res) {
 		})
 		var checkAllKillDone = function() {
 			console.log('killed '+pidCount+' out of '+pids.length);
-			if (pidCount == pids.length) {
+			if (pidCount === pids.length) {
 				res.write('Server Processes Killed \n');
-				var cmd = 'cd '+serverPath+'gameserver/orangebox/; ./srcds_run -game tf -autoupdate -maxplayers 24 +map cp_badlands &;'
+				var cmd = 'cd '+serverPath+'gameserver/orangebox/;./srcds_run -game tf -autoupdate -maxplayers 24 +map cp_badlands &;'
 				console.log('cmd',cmd)
 				exec(cmd,function (error, stdout, stderr) {	  
+					if (error) {
+						console.log('error running tf2',error)
+					}
 				});
-				res.end('Server Restarting');
+				res.write('\nServer Restarting');
+				res.end();
 			}
 		}
 	})
